dde65b39b0123c0238e3da86e1ab2068c5757b62
jackpritchard <jack.pritchard@sonovate.com>
2024-06-13 22:48:43 +0100

    feat: render steps

--- src/components/Recipe/Steps/index.tsx
+++ src/components/Recipe/Steps/index.tsx
@@ -1,27 +1,34 @@
 import { useRecipe } from "../context";
 
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
 
 export default function Steps() {
-  const { recipe } = useRecipe();
-  const { steps } = recipe;
+	const { recipe } = useRecipe();
+	const { steps } = recipe;
 
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="text-right">Step</TableHead>
-          <TableHead>Instructions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {steps.map((step, index) => (
-          <TableRow key={step.content} className="text-xl">
-            <TableCell className="text-right">{index + 1}</TableCell>
-            <TableCell>{step.content}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
+	return (
+		<Table>
+			<TableHeader>
+				<TableRow>
+					<TableHead className="text-right">Step</TableHead>
+					<TableHead>Instructions</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{steps.map((step, index) => (
+					<TableRow key={step} className="text-xl">
+						<TableCell className="text-right">{index + 1}</TableCell>
+						<TableCell>{step}</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
+	);
 }
