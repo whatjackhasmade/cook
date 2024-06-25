58a565413f93836221121231f8a061acf8c244b5
jackpritchard <jack.pritchard@sonovate.com>
2024-06-25 09:58:51 +0100

    feat: add green thai curry ingredients

--- src/data/recipes/green-thai-curry.ts
+++ src/data/recipes/green-thai-curry.ts
@@ -4,65 +4,111 @@ import { Recipe } from ".";
 const recipe: Recipe = {
 	slug: RecipeSlug.GreenThaiCurry,
 	title: "Green thai curry",
+	youtube: "https://youtu.be/rTK8MWYYLko?t=53",
 	ingredients: [
+		{
+			name: "Paste",
+			ingredients: [
+				{
+					name: "Galngal",
+					quantity: "1",
+					unit: MeasurementUnit.Teaspoons,
+				},
+				{
+					name: "Green serranos chillis",
+					quantity: "25",
+					unit: MeasurementUnit.Grams,
+				},
+				{
+					name: "Cloves of garlic",
+					quantity: "7",
+					unit: MeasurementUnit.Count,
+				},
+				{
+					name: "Coriander stalks",
+					quantity: "4",
+					unit: MeasurementUnit.Count,
+				},
+				{
+					name: "Ground cumin",
+					quantity: "2",
+					unit: MeasurementUnit.Grams,
+				},
+				{
+					name: "Ground coriander seeds",
+					quantity: "4",
+					unit: MeasurementUnit.Grams,
+				},
+				{
+					name: "Ground white pepper",
+					quantity: "3",
+					unit: MeasurementUnit.Grams,
+				},
+				{
+					name: "Small shallots",
+					quantity: "2",
+					unit: MeasurementUnit.Count,
+				},
+				{
+					name: "Lemongrass stalk",
+					quantity: "1",
+					unit: MeasurementUnit.Count,
+				},
+				{
+					name: "Kaffir lime leaves",
+					quantity: "3",
+					unit: MeasurementUnit.Count,
+				},
+			],
+		},
 		{
 			name: "Tins of coconut cream",
 			quantity: "2",
 			unit: MeasurementUnit.Count,
 		},
 		{
-			name: "Galngal",
-			quantity: "1",
-			unit: MeasurementUnit.Teaspoons,
+			name: "Thai basil leaves",
+			quantity: "10",
+			unit: MeasurementUnit.Count,
 		},
 		{
-			name: "Green serranos chillis",
-			quantity: "25",
+			name: "Palm sugar",
+			quantity: "40",
 			unit: MeasurementUnit.Grams,
-		},
-		{
-			name: "Cloves of garlic",
-			quantity: "7",
-			unit: MeasurementUnit.Count,
-		},
-		{
-			name: "Coriander stalks (with leaves if you cba to remove them)",
-			quantity: "4",
-			unit: MeasurementUnit.Count,
-		},
-		{
-			name: "Ground cumin",
-			quantity: "2",
-			unit: MeasurementUnit.Grams,
-		},
-		{
-			name: "Ground coriander",
-			quantity: "4",
-			unit: MeasurementUnit.Grams,
-		},
-		{
-			name: "Ground white pepper",
-			quantity: "3",
-			unit: MeasurementUnit.Grams,
-		},
-		{
-			name: "Small shallots",
-			quantity: "2",
-			unit: MeasurementUnit.Count,
-		},
-		{
-			name: "Lemongrass stalk",
-			quantity: "1",
-			unit: MeasurementUnit.Count,
-		},
-		{
-			name: "Kaffir lime leaves",
-			quantity: "3",
-			unit: MeasurementUnit.Count,
 		},
 		{
 			name: "Fish sauce",
-			quantity: "30",
+			quantity: "20",
+			unit: MeasurementUnit.Grams,
+		},
+		{
+			name: "Courgette",
+			quantity: "1",
+			unit: MeasurementUnit.Count,
+		},
+		{
+			name: "Bell pepper",
+			quantity: "1",
+			unit: MeasurementUnit.Count,
+		},
+		{
+			name: "Bowls worth of rice",
+			quantity: "1",
+			unit: MeasurementUnit.Count,
+		},
+		{
+			name: "Chestnut mushrooms",
+			quantity: "150",
+			unit: MeasurementUnit.Grams,
+		},
+		{
+			name: "Carrot",
+			quantity: "1",
+			unit: MeasurementUnit.Count,
+		},
+		{
+			name: "Vegetable stock",
+			quantity: "10",
 			unit: MeasurementUnit.Grams,
 		},
 	],
