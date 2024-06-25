af743a5fea759309f7e8f23096eab4b2f14cd39a
jackpritchard <jack.pritchard@sonovate.com>
2024-06-25 10:09:11 +0100

    feat: add red curry ingredients

--- src/data/recipes/red-thai-curry.ts
+++ src/data/recipes/red-thai-curry.ts
@@ -1,10 +1,30 @@
 import { RecipeSlug } from "@/enums";
 import { Recipe } from ".";
 
+import greenThaiCurry from "./green-thai-curry";
+
 const recipe: Recipe = {
 	slug: RecipeSlug.RedThaiCurry,
 	title: "Red thai curry",
-	ingredients: [],
+	ingredients: greenThaiCurry.ingredients.map((ingredient) => {
+		if ("ingredients" in ingredient && ingredient.name === "Paste") {
+			return {
+				...ingredient,
+				ingredients: ingredient.ingredients.map((ingredient) => {
+					if (ingredient.name === "Green serranos chillis") {
+						return {
+							...ingredient,
+							name: "Red chillis",
+						};
+					}
+
+					return ingredient;
+				}),
+			};
+		}
+
+		return ingredient;
+	}),
 	steps: [],
 	emojis: `🍛 🌶️ 🥥`,
 };
